beee6982af04180911de39ee70c74c3169f05991
Andrés Muñoz <andres.munoz.bra@gmail.com>
2025-02-24 19:12:23 -0300

    rename main class

--- src/main/java/cl/tenpo/tenpochallenge/TenpoChallengeApplication.java
+++ src/main/java/cl/tenpo/tenpochallenge/TenpoChallengeApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvcRestApiApplication {
+public class TenpoChallengeApplication {
   public static void main(String[] args) {
-    SpringApplication.run(MvcRestApiApplication.class, args);
+    SpringApplication.run(TenpoChallengeApplication.class, args);
   }
 }
